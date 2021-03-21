e6c32bd4d98c9c5c58bc623fe19c20d3c9b91efa
Vũ Khương Duy <vukhuongduy23@gmail.com>
2021-03-21 22:06:53 +0700

    Update

--- main.go
+++ main.go
@@ -2,51 +2,68 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"time"
 
+	"github.com/BurntSushi/toml"
+	"github.com/joho/godotenv"
 	nats "github.com/nats-io/nats.go"
 )
 
 var (
-	nc               *nats.Conn
-	messages         chan []byte
-	sub              *nats.Subscription
-	agentNatChannel  string
-	exportNatChannel string
+	nc           *nats.Conn
+	messages     chan []byte
+	sub          *nats.Subscription
+	agentNaChnl  string
+	exportNaChnl string
 )
+
+type ChanConfig struct {
+	Control string `toml:"control"`
+	Data    string `toml:"data"`
+}
+
+type AgentConf struct {
+	Channels ChanConfig `toml:"channels" json:"channels"`
+}
+
+type ExportConf struct {
+	Routes []Route `json:"routes" toml:"routes" mapstructure:"routes"`
+}
+
+type Route struct {
+	MqttTopic string `json:"mqtt_topic" toml:"mqtt_topic" mapstructure:"mqtt_topic"`
+	NatsTopic string `json:"nats_topic" toml:"nats_topic" mapstructure:"nats_topic"`
+	Workers   int    `json:"workers" toml:"workers" mapstructure:"workers"`
+}
 
 func init() {
 	var err error
-    
-    file := mainflux.Env(envConfigFile, defConfigFile)
-    if err := bootstrap.Bootstrap(bsConfig, logger, file); err != nil {
-		return c, errors.Wrap(errFetchingBootstrapFailed, err)
-	}
-
-	if bsc, err = agent.ReadConfig(file); err != nil {
-		return c, errors.Wrap(errFailedToReadConfig, err)
-	}
+	godotenv.Load(".env")
+	agentNaChnl = "channels/" + getAgentNatChnl() + "/messages/services/#"
+	exportNaChnl = getExportNatChnl()
 
 	nc, err = nats.Connect("localhost:4222")
 	if err != nil {
 		log.Fatalf(fmt.Sprintf("Connect to NAT error: : %s", err))
 	}
-	messages = make(chan []byte, 100)
 
-	sub, err = nc.SubscribeSync(agentNatChannel)
+	messages = make(chan []byte, 100)
+	sub, err = nc.SubscribeSync(agentNaChnl)
+	check(err)
 }
 
 func main() {
-	go subscribeNat(agentNatChannel)
+	go subscribeNat(agentNaChnl)
 	transferToNat()
 }
 
 func subscribeNat(chann string) {
 	defer sub.Unsubscribe()
-    fmt.Println("Begin subscribe NAT at topic " + chann)
+	fmt.Println("Begin subscribe NAT at topic " + chann)
 	for {
-		sub, _ = nc.SubscribeSync(chann)
 		m, err := sub.NextMsg(1000 * time.Millisecond)
 		if err == nil && m.Data != nil {
 			fmt.Println("Message: ", string(m.Data))
@@ -61,7 +78,7 @@ func transferToNat() {
 		case msg := <-messages:
 			msgStr := string(msg)
 			fmt.Println("Message: " + msgStr)
-			publishNat(exportNatChannel, msgStr)
+			publishNat(exportNaChnl, msgStr)
 		default:
 			fmt.Println("Waitting message nat.")
 			time.Sleep(500 * time.Millisecond)
@@ -71,4 +88,35 @@ func transferToNat() {
 
 func publishNat(chann string, mesg string) {
 	nc.Publish(chann, []byte(mesg))
+}
+
+func getAgentNatChnl() string {
+	file := os.Getenv("MF_AGENT_CONFIG_FILE")
+	dat, err := ioutil.ReadFile(file)
+	check(err)
+
+	var conf AgentConf
+	if _, err = toml.Decode(string(dat), &conf); err != nil {
+		check(err)
+	}
+	return conf.Channels.Control
+}
+
+func getExportNatChnl() string {
+	file := os.Getenv("MF_EXPORT_CONFIG_FILE")
+	dat, err := ioutil.ReadFile(file)
+	check(err)
+
+	var conf ExportConf
+	if _, err = toml.Decode(string(dat), &conf); err != nil {
+		check(err)
+	}
+	fmt.Println(conf.Routes[0].NatsTopic)
+	return "conf.Routes"
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
